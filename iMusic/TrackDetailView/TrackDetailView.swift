dc0ac42872ecd012ec04b5128631909ecf5afe59
Maksim Alekseev <piratmax@gmail.com>
2020-12-16 22:20:50 +0300

    made trackImageView animations in TrackDetailView

--- iMusic/TrackDetailView/TrackDetailView.swift
+++ iMusic/TrackDetailView/TrackDetailView.swift
@@ -13,6 +13,7 @@ import AVKit
 class TrackDetailView: UIView {
     
     //MARK: - Vars and constants
+    
     private let disposeBag = DisposeBag()
     let player: AVPlayer = {
         let avPlayer = AVPlayer()
@@ -35,11 +36,17 @@ class TrackDetailView: UIView {
     @IBOutlet weak var nextTrackButton: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
     
+    //MARK: - awakeFromNib
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        let scale: CGFloat = 0.8
+        trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        trackImageView.layer.cornerRadius = 5
+        
         currentTimeSlider.setThumbImage(#imageLiteral(resourceName: "Knob"), for: .normal)
+        
         setupDragDownButton()
         setupPlayPauseButton()
     }
@@ -51,6 +58,7 @@ class TrackDetailView: UIView {
         trackTitleLabel.text = viewModel.trackName
         artistNameLabel.text = viewModel.artistName
         playTrack(previewURL: viewModel.previewUrl)
+        monitorStartTime()
         guard let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600") else { return }
         
         trackImageView.webImage(string600, placeHolder: #imageLiteral(resourceName: "albumImagePlaceholderBig"))
@@ -63,7 +71,16 @@ class TrackDetailView: UIView {
         player.play()
     }
     
-    //MARK: - Setup Methods
+    private func monitorStartTime() {
+        
+        let time = CMTimeMake(value: 1, timescale: 3)
+        let times = [NSValue(time: time)]
+        player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
+            self?.enlargeTrackImageView()
+        }
+    }
+    
+    //MARK: - Setup control methods
     
     private func setupDragDownButton() {
         dragDownButton.rx.tap.subscribe(onNext: { [weak self] in
@@ -77,12 +94,44 @@ class TrackDetailView: UIView {
             if self?.player.timeControlStatus == .paused {
                 self?.player.play()
                 self?.playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+                self?.enlargeTrackImageView()
             } else {
                 self?.player.pause()
                 self?.playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+                self?.reduceTrackImageView()
             }
             
         }).disposed(by: disposeBag)
     }
+    
+    //MARK:- Animations
+    
+    private func enlargeTrackImageView() {
+        
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 1,
+                       options: .curveEaseOut,
+                       animations: { [weak self] in self?.trackImageView.transform = .identity },
+                       completion: nil)
+        
+    }
+    
+    private func reduceTrackImageView() {
+        
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 1,
+                       options: .curveEaseOut,
+                       animations: { [weak self] in
+                        let scale: CGFloat = 0.8
+                        self?.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+                       },
+                       completion: nil)
+        
+    }
+
 }
 
